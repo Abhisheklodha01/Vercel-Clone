80361a391bdcc8fa0c0d897a966f60e0c9b8a9cb
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-01 01:53:35 +0530

    created user registration functionality

---
+++ api-server/controllers/user.js
@@ -0,0 +1,52 @@
+const { PrismaClient } = require("@prisma/client");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const prisma = new PrismaClient({});
+
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+
+        const exitedUser = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+        });
+
+        if (exitedUser) {
+            return res.status(400).json({
+                success: false,
+                message: "Email alreay exists please try login",
+            });
+        }
+        const hashPassword = await bcryptjs.hash(password, 10);
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashPassword,
+            },
+        });
+        const accessToken = jwt.sign(
+            { id: newUser.id, email: newUser.email },
+            process.env.JWT_SECRET
+        );
+        return res.status(200).json({
+            success: true,
+            message: "Registration successful",
+            accessToken
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server error please try again after some time",
+        });
+    }
+};
