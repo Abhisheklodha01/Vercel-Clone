4080e7524985ccf8e2478a112c3a0631fff5deb6
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-01 02:09:34 +0530

    created middleware for auththentication

---
+++ api-server/middlewares/auth.middleware.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken')
+const { PrismaClient } = require('@prisma/client')
+
+
+const prisma = new PrismaClient({})
+
+const isAuththenticated = async (req, resizeBy, next) => {
+    const token = req.headers["authorization"]?.replace("Bearer ", "")
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized request"
+        })
+    }
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY)
+        if (!decodedToken) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Token"
+            })
+        }
+        const id = decodedToken.id
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid token"
+            })
+        }
+
+        req.user = user
+        next()
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "server error please try after some time"
+        })
+    }
+
+}
+
+module.exports = { isAuththenticated }
