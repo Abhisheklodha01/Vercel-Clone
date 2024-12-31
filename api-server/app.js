e802f156dc3529ff9c57017ac6c1ee0fb74b4c3c
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-01 02:18:12 +0530

    added cors

--- api-server/app.js
+++ api-server/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const projectRouter = require('./routes/project.route')
 const deploymentRouter = require('./routes/deployment.route')
 const userRouter = require('./routes/user.route')
+const cors = require('cors')
 
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
   return res.send("Working fine")
