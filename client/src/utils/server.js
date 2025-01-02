6c27f7dcc22b3ee3d09125a6cbe0e66af158340f
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-03 04:45:07 +0530

    added backend url

--- client/src/utils/server.js
+++ client/src/utils/server.js
@@ -1,1 +1,2 @@
-export const backendUrl = "http://localhost:9000/api"
+// export const backendUrl = "http://localhost:9000/api"
+export const backendUrl = "https://vercel-clone-tady.onrender.com/api"
