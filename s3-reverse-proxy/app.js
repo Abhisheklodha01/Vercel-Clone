5e568c5084e28b3c44a77e8b9696dfa21e27624e
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-03 04:40:12 +0530

    solving bugs

--- s3-reverse-proxy/app.js
+++ s3-reverse-proxy/app.js
@@ -11,19 +11,18 @@ const BASE_PATH = process.env.BASE_PATH
 const proxy = httpProxy.createProxy()
 
 app.use((req, res) => {
-    const url = req.url;
-    subDomain = url.split('/')[1]
+    const hostName = req.hostname;
+    const subDomain = hostName.split('.')[0]
 
     const resolveTo = `${BASE_PATH}/${subDomain}`
-    return proxy.web(req, res, { target: resolveTo, changeOrigin: true, secure: true })
+    return proxy.web(req, res, {target: resolveTo, changeOrigin: true})
 })
 
 proxy.on('proxyReq', (proxyReq, req, res) => {
     const url = req.url
-    if (url === '/') {
+    if(url === '/') {
         proxyReq.path += 'index.html'
     }
-    console.log(proxyReq.path);
 })
 
 
