394b34746dfbbed47472f1078260a6227da14a4b
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-02 22:11:22 +0530

    removed logs

--- s3-reverse-proxy/app.js
+++ s3-reverse-proxy/app.js
@@ -12,17 +12,11 @@ const proxy = httpProxy.createProxy()
 
 app.use((req, res) => {
     const hostName = req.hostname;
+    const subDomain = hostName.split('.')[0]
 
-    // Extract subdomain for nested domain scenarios
-    const subDomain = hostName.includes('.vercel-clone-ox05.onrender.com')
-        ? hostName.split('.')[0] // Extract `abhisheklodha` from the start
-        : 'default'; // Fallback for cases without the nested subdomain
-
-    console.log('Hostname:', hostName);
-    console.log('Extracted Subdomain:', subDomain);
-
-    const resolveTo = `${BASE_PATH}/${subDomain}`;
-    console.log('Resolved Target:', resolveTo);
+    const resolveTo = `${BASE_PATH}/${subDomain}`
+    console.log(BASE_PATH);
+    console.log(subDomain);
     return proxy.web(req, res, { target: resolveTo, changeOrigin: true, secure: true })
 })
 
