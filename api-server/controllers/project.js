54843c68a82b65d87533b56b3256ef61bab39a4b
Abhisheklodha01 <abhisheklodha512@gmail.com>
2025-01-02 21:16:25 +0530

    handled run time errors and created new controller for fetching projects

--- api-server/controllers/project.js
+++ api-server/controllers/project.js
@@ -5,10 +5,14 @@ const { generateSlug } = require('random-word-slugs')
 const prisma = new PrismaClient({})
 
 const createProject = async (req, res) => {
-
+    const {userId} = req.body
+    console.log(userId);
+    
     const schema = z.object({
         name: z.string(),
-        gitUrl: z.string()
+        gitUrl: z.string(),
+        userId: z.number(),
+        subDomain: z.string()
     })
 
     const safeParseResult = schema.safeParse(req.body)
@@ -18,19 +22,48 @@ const createProject = async (req, res) => {
         })
     }
 
-    const { name, gitUrl } = safeParseResult.data
+    try {
+        const { name, gitUrl, userId, subDomain } = safeParseResult.data
 
     const project = await prisma.project.create({
         data: {
-            name,
+            name: name,
             gitUrl,
-            subDomain: generateSlug()
+            userId,
+            subDomain: subDomain.trim()
         }
     })
     return res.status(201).json({
         message: "Project Creation Successful",
         project
     })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Error in Project Creation",
+        })
+    }
 }
 
-module.exports = { createProject }
+const getUserProjects = async (req, res) => {
+    const userId = req.params.id 
+    console.log(userId);
+    
+    try {
+        const projects = await prisma.project.findMany({
+            where : {
+                userId: Number(userId)
+            }
+        })
+        return res.status(200).json({
+            message: "Projects find successfully",
+            projects
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server error",
+        })
+    }
+}
+
+module.exports = { createProject, getUserProjects }
